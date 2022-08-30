267d3b1edd39caacf1931bcc156b33a73516cfa5
Qousai Edelbi <77577864+Networkingskills@users.noreply.github.com>
2022-08-30 17:01:03 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    # organization = "AdaptiveSolutions-Dev"
+    organization = "AdaptiveSolutions-Dev"
     workspaces {
       name = "ace-iac-day-zero"
     }
